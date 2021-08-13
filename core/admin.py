8c531d6859c36b7fa98e42eae315df6bc4972561
Advalker <Advalker@hotmail.com>
2021-08-13 07:00:45 -0300

    Criando cargos

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from . models import Cargo, Servicos, Funcionario
+
+
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+
+@admin.register(Servicos)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+
+@admin.register(Funcionario)
+class Funcionario(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
